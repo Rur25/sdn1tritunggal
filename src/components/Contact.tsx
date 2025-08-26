c1ac613a79c773370f2609e950f95476cf21f233
Rur25 <136113758+Rur25@users.noreply.github.com>
2025-08-27 01:48:52 +0700

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -282,7 +282,7 @@ const Contact = () => {
                 {/* Google Maps Embed */}
                 <div className="mb-6">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.2389972833225!2d112.2544089091354!3d-7.098274269560109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77f32576702b31%3A0x4fdce865797fe7b4!2sSD%20Negeri%201%20Tritunggal!5e0!3m2!1sid!2sid!4v1756234000955!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.2389972833225!2d112.2544089091354!3d-7.098274269560109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77f32576702b31%3A0x4fdce865797fe7b4!2sSD%20Negeri%201%20Tritunggal!5e0!3m2!1sid!2sid!4v1756234000955!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     width="100%"
                     height="300"
                     style={{ border: 0 }}
